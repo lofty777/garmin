150c53bb6dcbefbe15a94ca4052948e7c7cc0a06
Lofty777 <meshkov88@gmail.com>
2022-07-23 15:41:57 +0300

    add tiny-slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,28 @@
-$(document).ready(function(){
+/* $(document).ready(function(){
 	$('.carousel__inner').slick({        
-    speed: 300,    
-    adaptiveHeight: true,
-    prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+		speed: 1200,    
+		adaptiveHeight: true,
+		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {          
-          dots: true,
-          arrows: false
-        }
-      }
-    ]
-  });
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {          
+					dots: true,
+					arrows: false
+				}
+			}
+		]
+	});
+}); */
+
+var slider = tns({
+	container: '.carousel__inner',
+	items: 1,
+	slideBy: 'page',
+	autoplay: false,
+	controlsText: [
+		'<img src="icons/left.svg">',
+		'<img src="icons/right.svg">'		
+	]
 });
